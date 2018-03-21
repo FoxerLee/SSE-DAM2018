ec913c3d34c8649b2f52f17be70585ad75175b47
FoxerLee <foxerlee1@gmail.com>
2018-03-21 23:33:38 +0800

    add code comment

--- hw1/q1/q1.py
+++ hw1/q1/q1.py
@@ -7,8 +7,8 @@ np.set_printoptions(threshold=np.inf)
 
 def get_data():
     """
-
-    :return:
+    利用pandas包，从csv文件中提取所需数据，聚合后转换为矩阵格式
+    :return: datas_set为DataFrame格式的矩阵和datas_matrix为array格式的矩阵
     """
     datas = pd.read_csv('trade.csv', usecols=['vipno', 'pluno', 'amt'])
 
@@ -35,25 +35,31 @@ def get_data():
 
 def lsh(p_hash_size, distance_func):
     """
-
-    :param p_hash_size:
-    :param distance_func:
-    :return:
+    实现局部敏感哈希模拟KNN的具体函数
+    :param p_hash_size: 与vipno的总数（去重后）相乘构成最终的hash_size
+    :param distance_func: 可选择的距离计算函数
+    :return: 去除自身之后的该vipno对应knn的输出vipno
     """
     datas_set, datas_matrix = get_data()
+    # vipno_nums 为vipno去重后的总数
     vipno_nums = len(datas_matrix[0])
+    # 初始化lshash
     lsh = LSHash(int(vipno_nums * p_hash_size), len(datas_matrix[:, 0]))
     for i in range(vipno_nums):
+        # extra_data为当前列对应的vipno值，作为之后输出的时候所想要的knn的输出vipno
         lsh.index(datas_matrix[:, i], extra_data=datas_set.columns[i])
 
+    # 随机取一个vipno（这里是vipno对应的下标）
     random_vipno = random.randint(0, vipno_nums-1)
 
     print("hash size: {}".format(vipno_nums*p_hash_size))
     print("input vipno: {}".format(datas_set.columns[random_vipno]))
     vipno_res = []
+    # num_results可以限制输出的结果个数，这里取前6个，因为第一个为输入列本身
     for res in lsh.query(datas_matrix[:, random_vipno], num_results=6, distance_func=distance_func):
         vipno_res.append(res[0][1])
     print("knn output(from 1 to 5): {}".format(vipno_res[1:]))
+    return vipno_res[1:]
 
 
 if __name__ == '__main__':
